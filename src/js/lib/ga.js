3ea029c2b798664de4589658a1d529bd96ed81fc
Matt Merryfull <matt.merryfull@neonlight.io>
2022-04-01 17:40:26 +1100

    screen shot capability added to getUserMedia, manual and interval trigger. Basic s3 functionality plumbed in.

--- src/js/lib/ga.js
+++ src/js/lib/ga.js
@@ -5,5 +5,5 @@ i['GoogleAnalyticsObject']=r; i[r]=i[r]||function() {
   m=s.getElementsByTagName(o)[0]; a.async=1; a.src=g; m.parentNode.insertBefore(a, m);
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-48530561-1', 'auto');
-ga('send', 'pageview');
+// ga('create', 'UA-48530561-1', 'auto');
+// ga('send', 'pageview');
